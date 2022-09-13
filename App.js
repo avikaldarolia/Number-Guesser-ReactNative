8735db129a16583df4a6e801a997f99ebcc80db3
Avikal Darolia <avikaldarolia7@gmail.com>
2022-09-13 18:10:22 +0530

    Background image added over the gradient

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from "expo-linear-gradient"
 
 import StartGameScreen from './screens/StartGameScreen';
@@ -8,7 +8,13 @@ import StartGameScreen from './screens/StartGameScreen';
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground source={require('./assets/images/background.png')}
+       resizeMode="cover" 
+       style={styles.rootScreen}
+       imageStyle={styles.backgroundImage}
+       >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -17,10 +23,7 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1
   },
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
+   backgroundImage: {
+    opacity: 0.15
+   }
 });

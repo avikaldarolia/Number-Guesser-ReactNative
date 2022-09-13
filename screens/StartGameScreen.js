e55331f9f45f392cc61717ef8538789eb2e19a43
Avikal Darolia <avikaldarolia7@gmail.com>
2022-09-13 17:15:30 +0530

    Shadow Style added

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,31 @@
+import { TextInput , View , StyleSheet} from "react-native"
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput />
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+    )
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: "#72063c",
+        borderRadius: 8,
+        // only for android
+        elevation: 10,
+        // only for ios
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    }
+})

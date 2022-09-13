c4e6c93f7d27225ca98896c67b421b52b09104d4
Avikal Darolia <avikaldarolia7@gmail.com>
2022-09-13 19:11:24 +0530

    Colors.js file added for easier use of Colors

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native"
+import Colors from "../constants/colors";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen({onPickNumber}) {
@@ -47,7 +48,7 @@ const styles = StyleSheet.create({
         padding: 16,
         marginTop: 100,
         marginHorizontal: 24,
-        backgroundColor: "#3b021f",
+        backgroundColor: Colors.primary800,
         borderRadius: 8,
         // only for android
         elevation: 10,
@@ -61,9 +62,9 @@ const styles = StyleSheet.create({
         height: 50,
         width: 50,
         fontSize: 32,
-        borderBottomColor: "#ddb52f",
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 2,
-        color: "#ddb52f",
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: "bold",
         textAlign: "center"

8f0b0ac5f32bf22d8083447734254436c90cd685
Avikal Darolia <avikaldarolia7@gmail.com>
2022-09-13 18:43:23 +0530

    Switching screen dynamically

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native"
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({onPickNumber}) {
     const [enteredNumber, setEnteredNumber] = useState("")
 
     function numberInputHandler(enteredText) {
@@ -20,7 +20,7 @@ function StartGameScreen() {
             [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}])
             return;
         }
-        console.log("Valid Number");
+        onPickNumber(enteredNumber)
     }
 
     return (
